07eebc1900101a3698931296f4c5bd0934bbebeb
Consecratio <jcasique@gmail.com>
2021-08-02 10:46:49 -0700

    added more algos

---
+++ houseComp.py
@@ -0,0 +1,9 @@
+'''
+Write a function that takes two values, an array (arr) and an int (days).
+The array will have only 0's and 1's [1, 0, 0, 0, 1, 0, 0]
+And the days will be positive numbers > 0.
+
+Each day check each number's neighbors, if they're the same (both 0's or both 1's)
+then the number will change to 0.
+If they are not the same, the number will change to 1.
+'''

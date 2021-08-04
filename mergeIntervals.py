40d58f8ee51d5895e639082c03131eeb31f71328
Consecratio <jcasique@gmail.com>
2021-08-03 18:37:21 -0700

    added solution branch

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -1,14 +1,17 @@
 '''
 SIMILAR
-----------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 https://leetcode.com/problems/meeting-rooms/
 https://leetcode.com/problems/meeting-rooms-ii/
 https://leetcode.com/problems/merge-intervals/
 
 PROMPT
-----------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
+Given two arrays (arrival times, duration times) of meetings, write a function
+to return the max number of meetings that can be held without overlapping.
+
 Find number of max meetings that can be held.
-This test should output 4
-[978, 409, 229, 934, 299, 982, 636, 14, 866, 815, 64, 537, 426, 670, 116, 95, 630]
-[502, 518, 196, 106, 405, 452, 299, 189, 124, 506, 883, 753, 567, 717, 338, 439, 145]
+arrival times = [978, 409, 229, 934, 299, 982, 636, 14, 866, 815, 64, 537, 426, 670, 116, 95, 630]
+duration times = [502, 518, 196, 106, 405, 452, 299, 189, 124, 506, 883, 753, 567, 717, 338, 439, 145]
+This test should return 4
 '''

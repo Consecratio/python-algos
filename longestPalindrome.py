9576b219a8b6d61b40fb08074537b3d6941ab059
Consecratio <jcasique@gmail.com>
2021-08-11 16:31:06 -0700

    added solution for longest palindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -10,7 +10,22 @@ Given a string s, return the longest palindromic substring in s.
 import math
 
 def longestPalindrome(s):
-    pass
+    res = ""
+    for i in range(len(s)):
+        # odd case, like "aba"
+        tmp = helper(s, i, i)
+        if len(tmp) > len(res):
+            res = tmp
+        # even case, like "abba"
+        tmp = helper(s, i, i+1)
+        if len(tmp) > len(res):
+            res = tmp
+    return res
+
+def helper(s, l, r):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        l -= 1; r += 1
+    return s[l+1:r]
 
 print(longestPalindrome("babad")) # Output: "bab" OR "aba"
 print(longestPalindrome("cbbd")) # Output: "bb"

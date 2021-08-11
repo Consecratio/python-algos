037ab722785a6724c53f8173db870f0a887be9aa
Consecratio <jcasique@gmail.com>
2021-08-11 15:50:05 -0700

    starting solutions

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -7,11 +7,12 @@ PROMPT
 -------------------------------------------------------------------------
 Given a string s, return the longest palindromic substring in s.
 '''
+import math
 
 def longestPalindrome(s):
     pass
 
-print(longestPalindrome("babad")) # Output: "bab"
+print(longestPalindrome("babad")) # Output: "bab" OR "aba"
 print(longestPalindrome("cbbd")) # Output: "bb"
 print(longestPalindrome("a")) # Output: "a"
 print(longestPalindrome("ac")) # Output: "a"

f4c3b07ec43b4606058afecf43ebe7fdd246f7f5
Consecratio <jcasique@gmail.com>
2021-08-11 15:14:36 -0700

    added longest palindrome

---
+++ longestPalindrome.py
@@ -0,0 +1,17 @@
+'''
+SIMILAR
+-------------------------------------------------------------------------
+https://leetcode.com/problems/longest-palindromic-substring/
+
+PROMPT
+-------------------------------------------------------------------------
+Given a string s, return the longest palindromic substring in s.
+'''
+
+def longestPalindrome(s):
+    pass
+
+print(longestPalindrome("babad")) # Output: "bab"
+print(longestPalindrome("cbbd")) # Output: "bb"
+print(longestPalindrome("a")) # Output: "a"
+print(longestPalindrome("ac")) # Output: "a"

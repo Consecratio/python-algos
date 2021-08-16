333581594eaff439d98d3953dbc5434c1393ffb6
Consecratio <jcasique@gmail.com>
2021-08-16 11:32:23 -0700

    memoized nth_stair solution

--- nth_stair.py
+++ nth_stair.py
@@ -7,3 +7,20 @@ PROMPT
 -------------------------------------------------------------------------
 A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
 '''
+
+def solution(n, memo = {}):
+    if n in memo:
+        return memo[n]
+    if n == 1 or n == 0:
+        return 1
+    elif n == 2:
+        return 2
+    
+    memo[n] = solution(n - 3, memo) + solution(n - 2, memo) + solution(n - 1, memo)
+
+    return memo[n]
+
+
+print(solution(4)) # should return 7
+print(solution(3)) # should return 4
+print(solution(40))

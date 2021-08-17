13b88eacd6bf5abc77214c6b272855ec2a4c3259
Consecratio <jcasique@gmail.com>
2021-08-17 14:35:30 -0700

    added maze.py

---
+++ maze.py
@@ -0,0 +1,25 @@
+'''
+SIMILAR
+-------------------------------------------------------------------------
+
+
+PROMPT
+-------------------------------------------------------------------------
+Given a Maze array (2D Array), a startRow, startCol, endRow, and endCol. Output if there is a path from the start location
+to the end location. Return True if a path can be found, return False if no path exists.
+
+0 = open path
+1 = wall
+
+Maze = [
+    [0,0,1,1,1],
+    [1,0,0,0,1],
+    [0,0,1,0,1],
+    [1,1,0,0,1],
+]
+
+startRow = 1
+startCol = 1
+endRow = 3
+endCol = 2
+'''

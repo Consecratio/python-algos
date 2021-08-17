c5de28ee31e6f610b13bc98e21f2a0b0c3069b4f
Consecratio <jcasique@gmail.com>
2021-08-17 15:21:04 -0700

    working on maze solution, still not figured out

--- maze.py
+++ maze.py
@@ -24,15 +24,41 @@ endRow = 3
 endCol = 2
 '''
 
-def mazeFinder(maze, startRow, startCol, endRow, endCol):
-    print(maze)
-    print(startRow)
-    print(startCol)
-    print(endRow)
-    print(endCol)
+def mazeFinder(maze, startRow, startCol, endRow, endCol, memo = {}):
+    if len(maze) == 0: return False
+    if f"{endRow}{endCol}" not in memo: 
+        memo[f'{endRow}{endCol}'] = False
+    elif memo[f"{endRow}{endCol}"] is True:
+        # base case for finding the end of the maze
+        return memo
 
-    return 0
+    
+    # N
+    if startRow > 0 and memo[f"{endRow}{endCol}"] == False:
+        if maze[startRow - 1][startCol] == 0:
+            memo = mazeFinder(maze, startRow - 1, startCol, endRow, endCol)
+    # E
+    if startCol < len(maze[startCol]) - 1 and memo[f"{endRow}{endCol}"] == False:
+        if maze[startRow][startCol + 1] == 0:
+            memo = mazeFinder(maze, startRow, startCol + 1, endRow, endCol)
+    # S
+    if startRow < len(maze) - 1:
+        if maze[startRow + 1][startCol] == 0 and memo[f"{endRow}{endCol}"] == False:
+            memo = mazeFinder(maze, startRow + 1, startCol, endRow, endCol)
+    # W
+    if startCol > 0:
+        if maze[startRow][startCol - 1] == 0 and memo[f"{endRow}{endCol}"] == False:
+            memo = mazeFinder(maze, startRow, startCol - 1, endRow, endCol)
+    
+    if memo[f"{endRow}{endCol}"] is True:
+        # base case for finding the end of the maze
+        return memo
 
+    return memo
+
+'''
+CODE ABOVE THIS AREA
+'''
 
 maze = [
     [0,0,1,1,1],
@@ -43,7 +69,7 @@ maze = [
 
 startRow = 1
 startCol = 1
-endRow = 3
-endCol = 2
+endRow = 2
+endCol = 3
 
 print(mazeFinder(maze, startRow, startCol, endRow, endCol))

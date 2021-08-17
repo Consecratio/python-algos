e52dd2888f9a0a9dadf06ac57e838f44880b6b6a
Consecratio <jcasique@gmail.com>
2021-08-17 13:46:02 -0700

    added explanation to gcd.py

--- gcd.py
+++ gcd.py
@@ -1,3 +1,13 @@
+'''
+SIMILAR
+-------------------------------------------------------------------------
+https://www.codewars.com/kata/5849169a6512c5964000016e
+
+PROMPT
+-------------------------------------------------------------------------
+Write a function that returns the greatest common factor of an array of positive integers. Your return value should be a number, you will only receive positive integers.
+'''
+
 import math
 
 def generalizedGCD(num, arr):

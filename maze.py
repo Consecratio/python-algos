49a4604ad8469e87a0d3441274ececaa812bcb5c
Consecratio <jcasique@gmail.com>
2021-08-17 17:02:57 -0700

    solved maze

--- maze.py
+++ maze.py
@@ -13,9 +13,9 @@ to the end location. Return True if a path can be found, return False if no path
 
 Maze = [
     [0,0,1,1,1],
-    [1,0,0,0,1],
-    [0,0,1,0,1],
-    [1,1,0,0,1],
+    [0,0,0,0,1],
+    [0,1,1,1,1],
+    [0,0,0,0,1],
 ]
 
 startRow = 1
@@ -24,36 +24,56 @@ endRow = 3
 endCol = 2
 '''
 
-def mazeFinder(maze, startRow, startCol, endRow, endCol, memo = {}):
+def mazeFinder(maze, startRow, startCol, endRow, endCol):
     if len(maze) == 0: return False
-    if f"{endRow}{endCol}" not in memo: 
-        memo[f'{endRow}{endCol}'] = False
-    elif memo[f"{endRow}{endCol}"] is True:
-        # base case for finding the end of the maze
+
+    found_exit = travel(maze, startRow, startCol, endRow, endCol)
+
+    return found_exit
+
+def travel(maze, startRow, startCol, endRow, endCol, memo = {}):
+    first_call = True
+
+    # base case
+    if startRow == endRow and startCol == endCol:
+        print("FOUND THE END")
+        memo[f"{endRow}{endCol}"] = True
         return memo
 
+    # already been here
+    if f"{startRow}{startCol}" in memo:
+        return memo
+
+    # start with end location
+    if f"{endRow}{endCol}" not in memo:
+        memo[f"{endRow}{endCol}"] = False
+    else:
+        first_call = False
+ 
+    # add current location as traveled
+    memo[f"{startRow}{startCol}"] = False
     
     # N
     if startRow > 0 and memo[f"{endRow}{endCol}"] == False:
         if maze[startRow - 1][startCol] == 0:
-            memo = mazeFinder(maze, startRow - 1, startCol, endRow, endCol)
+            memo = travel(maze, startRow - 1, startCol, endRow, endCol, memo)
     # E
-    if startCol < len(maze[startCol]) - 1 and memo[f"{endRow}{endCol}"] == False:
+    if startCol < len(maze[startRow]) - 1 and memo[f"{endRow}{endCol}"] == False:
         if maze[startRow][startCol + 1] == 0:
-            memo = mazeFinder(maze, startRow, startCol + 1, endRow, endCol)
+            memo = travel(maze, startRow, startCol + 1, endRow, endCol, memo)
     # S
-    if startRow < len(maze) - 1:
-        if maze[startRow + 1][startCol] == 0 and memo[f"{endRow}{endCol}"] == False:
-            memo = mazeFinder(maze, startRow + 1, startCol, endRow, endCol)
+    if startRow < len(maze) - 1 and memo[f"{endRow}{endCol}"] == False:
+        if maze[startRow + 1][startCol] == 0:
+            memo = travel(maze, startRow + 1, startCol, endRow, endCol, memo)
     # W
-    if startCol > 0:
-        if maze[startRow][startCol - 1] == 0 and memo[f"{endRow}{endCol}"] == False:
-            memo = mazeFinder(maze, startRow, startCol - 1, endRow, endCol)
-    
-    if memo[f"{endRow}{endCol}"] is True:
-        # base case for finding the end of the maze
-        return memo
+    if startCol > 0 and memo[f"{endRow}{endCol}"] == False:
+        if maze[startRow][startCol - 1] == 0:
+            memo = travel(maze, startRow, startCol - 1, endRow, endCol, memo)
 
+    if first_call == True:
+        print(memo)
+        return memo[f"{endRow}{endCol}"]
+    
     return memo
 
 '''
@@ -61,10 +81,10 @@ CODE ABOVE THIS AREA
 '''
 
 maze = [
-    [0,0,1,1,1],
-    [1,0,0,0,1],
-    [0,0,1,0,1],
-    [1,1,0,0,1],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
 ]
 
 startRow = 1

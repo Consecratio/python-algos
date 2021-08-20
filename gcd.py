fa52fc43391404e048386118b3f7d7544d7bf36d
Consecratio <jcasique@gmail.com>
2021-08-19 19:18:42 -0700

    added more description to gcd.py

--- gcd.py
+++ gcd.py
@@ -6,6 +6,15 @@ https://www.codewars.com/kata/5849169a6512c5964000016e
 PROMPT
 -------------------------------------------------------------------------
 Write a function that returns the greatest common factor of an array of positive integers. Your return value should be a number, you will only receive positive integers.
+
+You will receive an integer = len(array)
+and an array of numbers.
+
+Example:
+generalizedGCD(5, [10, 8, 6, 4, 2])
+
+Should return:
+2
 '''
 
 import math

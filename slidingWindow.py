d4c15fc4f4e161d98eb5371598b3f8f1011c7940
Consecratio <jcasique@gmail.com>
2021-08-20 20:19:14 -0700

    added starter code

--- slidingWindow.py
+++ slidingWindow.py
@@ -9,3 +9,13 @@ Parking spots [2, 3, 7, 15]
 Number of cars that need to be covered k = 3
 Find smallest length needed to cover k cars
 '''
+
+def slidingWindow(spots, k):
+    pass
+
+
+'''
+CODE ABOVE HERE
+'''
+
+print([2, 3, 7, 15], 3)

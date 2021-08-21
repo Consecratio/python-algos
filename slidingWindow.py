2897d1fa3f4525310ac36c06e1fee112cbecb43b
Consecratio <jcasique@gmail.com>
2021-08-20 20:19:39 -0700

    added starter code

--- slidingWindow.py
+++ slidingWindow.py
@@ -1,11 +1,21 @@
 '''
 SIMILAR
--------------------------------------------------------------------------
+----------------------------------------------------------
 https://leetcode.com/discuss/general-discussion/657507/Sliding-Window-for-Beginners-Problems-or-Template-or-Sample-Solutions
 
 PROMPT
--------------------------------------------------------------------------
+----------------------------------------------------------
 Parking spots [2, 3, 7, 15]
 Number of cars that need to be covered k = 3
-Find and return the smallest length needed to cover k cars
+Find smallest length needed to cover k cars
 '''
+
+def slidingWindow(spots, k):
+    pass
+
+
+'''
+CODE ABOVE HERE
+'''
+
+print([2, 3, 7, 15], 3)

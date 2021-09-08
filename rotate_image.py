83ed5bd1911fa134129074a0565a1d5047f4321f
Consecratio <jcasique@gmail.com>
2021-09-08 13:03:06 -0700

    added rotate_image.py

---
+++ rotate_image.py
@@ -0,0 +1,26 @@
+'''
+SIMILAR
+----------------------------------------------------------
+
+
+PROMPT
+----------------------------------------------------------
+Given an array that represents an image, rotate the image
+by 90 degrees, clockwise.
+
+Example:
+
+image = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+returns:
+
+new_image = [
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+]
+'''
